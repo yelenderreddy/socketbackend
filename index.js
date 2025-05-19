bb117968a32fd3a94d20349c2814396f3a6369db
yelenderreddy <148934745+yelenderreddy@users.noreply.github.com>
2025-05-19 17:19:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // React app origin
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected: " + socket.id);
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data); // Broadcast to all
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: " + socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
