0ae5c1ca4b7b00755f74522e425d687abaa32697
yelenderreddy <148934745+yelenderreddy@users.noreply.github.com>
2025-05-20 10:24:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // React app origin
+    origin: "https://socketfrontend-production-69da.up.railway.app", // React app origin
     methods: ["GET", "POST"]
   }
 });
